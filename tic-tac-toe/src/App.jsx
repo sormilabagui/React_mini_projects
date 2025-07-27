48fd2ccf464e23c664aa8085c2e901f6cde34c32
sormilabagui <sumontabagui189@gmail.com>
2025-07-27 21:23:02 +0530

    Add files via upload

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Tictactoe from "./components/tictactoe/tictiactoe.jsx";
+
+
+function App() {
+
+  return (
+      <div>
+        <Tictactoe />
+      </div>
+        
+      
+  )
+}
+
+export default App
